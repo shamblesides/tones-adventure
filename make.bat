69cd73a6c7e00e8bab5cbe073b67e70c2dc0e98b
Nigel Nelson <nigel.mark.nelson@gmail.com>
2017-05-11 11:40:28 -0600

    fix; song.o doesn't exist in this version

--- make.bat
+++ make.bat
@@ -4,6 +4,6 @@ rem main code stuff goes in bank 0 (no -Wf-boX argument!!)
 ..\gbdk\bin\lcc -c -o o\next.o next.c
 
 rem MBC1, 8 rom banks, GBC support
-..\gbdk\bin\lcc -Wl-yt1 -Wl-yo8 -Wl-yp0x143=0x80 -o next.gb o\areas.o o\chunks.o o\next.o o\tone.o o\crab.o o\merm.o o\wizard.o o\scyfe.o o\bkg_tiles.o o\intro.o o\song1.o
+..\gbdk\bin\lcc -Wl-yt1 -Wl-yo8 -Wl-yp0x143=0x80 -o next.gb o\areas.o o\chunks.o o\next.o o\tone.o o\crab.o o\merm.o o\wizard.o o\scyfe.o o\bkg_tiles.o o\intro.o
 
 ..\bgb\bgb next.gb
